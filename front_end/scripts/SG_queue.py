75b69c23493c252e565b1ee1892ab073913963bb
lzixuan <lzixuan@umich.edu>
2022-12-05 16:42:28 -0500

    sg queue + transformation matrices + bbox transformation

--- front_end/scripts/SG_queue.py
+++ front_end/scripts/SG_queue.py
@@ -1,3 +1,6 @@
+import math
+import numpy
+
 MIN_OBJS = 10
 SPATIAL_DIS = 5
 
@@ -8,16 +11,25 @@ class BoundingBox:
         pass
 
     def center():
-        pass
+        return [(corners[0][0] + corners[6][0])/2, (corners[0][1] + corners[6][1])/2, (corners[0][2] + corners[6][2])/2] 
+        
 
 class SG_queue:
     def __init__(self) -> None:
-        self.sg_q = []
+        self.sg_q = [] # list of bounding boxes in the global frame
 
         pass
 
     def insert(new_queue):
+        while (len(new_queue) != 0):
+            item = new_queue.pop(0)
+            for existing_item in sg_q:
+                if (item.label == existing_item.label):
+                    if (overlap(item, existing_item) > 0.8):
+                        sg_q.append(item)
         pass
 
     def update(curr_pose):
+        
+
         pass
--- front_end/scripts/utils.py
+++ front_end/scripts/utils.py
@@ -1,8 +1,33 @@
-def overlap():
+import math
+import transformation
+
+def overlap(bbox1, bbox2):
     pass
 
 def transform_pcd(pcd, pose):
+    '''
+    pcd: an array of points representing a point cloud in camera's frame
+    pose: the camera's position and orientation in global frame
+    return the pcd with the orientation transformed into the camera's frame (not the position)
+    '''
     pass
 
 def transform_bbox(bbox, pose):
-    pass
+    '''
+    bbox: an object with an array of 8 corners (8 points) and a label in camera's frame
+    pose: the camera's position and orientation in global frame
+    return the bounding box with corners transformed into the global frame
+    '''
+    camera_transform = transformation.quaternion_matrix(pose.orientation)
+    for corner in bbox.corners:
+        point_transform = transformation.translation_matrix(corner)
+        transformed_corner_matrix = numpy.matmul(camera_transform, point_transform)
+        corner = [transformed_corner_matrix[0][3], transformed_corner_matrix[1][3], transformed_corner_matrix[2][3]]
+
+    return bbox
+
+def distance(point1, point2):
+    '''
+    point1, point2: array of size 3 points representing a point [x,y,z]
+    '''
+    return math.sqrt( (point1[0] - point2[0])**2 + (point1[1] - point2[1])**2 + (point1[2] - point2[2])**2 )
