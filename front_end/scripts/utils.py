b94075e260211e3d2914aace15e077f7df9e58a6
lzixuan <lzixuan@umich.edu>
2022-12-06 15:42:13 -0500

    transform pcd

--- front_end/scripts/utils.py
+++ front_end/scripts/utils.py
@@ -3,6 +3,12 @@ import numpy as np
 import transformation
 
 def overlap(bbox1, bbox2):
+    '''
+    Given two bounding box object, find the estimated overlap percentage
+    '''
+
+
+
     pass
 
 def adjacent_edge(prior, posterior):
@@ -10,11 +16,19 @@ def adjacent_edge(prior, posterior):
 
 def transform_pcd(pcd, pose):
     '''
-    pcd: an array of points representing a point cloud in camera's frame
+    pcd: an array of points representing a point cloud in camera's frame [n x 3]
     pose: the camera's position and orientation in global frame
     return the pcd with the orientation transformed into the camera's frame (not the position)
     '''
-    pass
+    camera_transform = transformation.quaternion_matrix(pose.orientation)
+    new_pcd = []
+    for point in pcd:
+        point_transform = transformation.translation_matrix(point)
+        transformed_point_matrix = np.matmul(camera_transform, point_transform)
+        new_point = [transformed_point_matrix[0][3], transformed_point_matrix[1][3], transformed_point_matrix[2][3]]
+        new_pcd.append(new_point)
+
+    return new_pcd
 
 def transform_bbox(bbox, pose):
     '''
@@ -22,11 +36,14 @@ def transform_bbox(bbox, pose):
     pose: the camera's position and orientation in global frame
     return the bounding box with corners transformed into the global frame
     '''
-    camera_transform = transformation.quaternion_matrix(pose.orientation)
+    camera_transform = transformation.quaternion_matrix(pose.position)
+    new_corners = []
     for corner in bbox.corners:
         point_transform = transformation.translation_matrix(corner)
         transformed_corner_matrix = np.matmul(camera_transform, point_transform)
-        corner = [transformed_corner_matrix[0][3], transformed_corner_matrix[1][3], transformed_corner_matrix[2][3]]
+        new_corner = [transformed_corner_matrix[0][3], transformed_corner_matrix[1][3], transformed_corner_matrix[2][3]]
+        new_corners.append(new_corner)
+    bbox.corners = new_corners
 
     return bbox
 
